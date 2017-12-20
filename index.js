9314192e4ff4a8558e7a5924b611c8f3ec470a1a
Salvo <salvo.cannamela@gmail.com>
2017-12-20 22:50:12 +0000

    Implement some better api response

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
 const port = process.env.PORT || 3000;
 
-const readability = require('node-readability');
+const read = require('node-readability');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const validUrl = require('valid-url');
+const urllib = require('url');
+const htmlclean = require('htmlclean');
+const sanitizeHtml = require('sanitize-html');
+const sanitize = (html) => sanitizeHtml(htmlclean(html), {
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img' ])
+});
 
 app.listen(port);
 app.set('etag', false);
 app.use(bodyParser.json());
-app.route('/get')
+
+app.route('/parser')
     .get((req, res) => {
         console.log('GET');
-        console.log(req.body);
-        res.json({json: 'YAS'});
+        const url = req.query.url;
+        if (url && validUrl.isUri(url)) {
+            const parsedURL = urllib.parse(url);
+
+            read(url, (err, article, meta) => {
+                if (err) {
+                    res.json({ err });
+                    return;
+                }
+
+                const content = sanitize(article.content);
+                const title = article.title;
+
+                // Close article to clean up jsdom and prevent leaks
+                article.close();
+
+                res.json({
+                    url,
+                    title,
+                    content,
+                    domain: parsedURL.hostname,
+                    // date_published: null,
+                    // lead_image_url: null,
+                });
+            });
+        } else {
+            error(res, `Not a valid URL supplied`);
+        }
     })
     .post((req, res) => {
         console.log('GET');
@@ -23,24 +56,7 @@ app.route('/get')
             if (validUrl.isUri(url)) {
                 res.json({ message: `Let's go parse ${url}` });
 
-                readability(url, (err, article, meta) => {
 
-                    // Main Article
-                    console.log(article.content);
-                    // Title
-                    console.log(article.title);
-
-                    // HTML Source Code
-                    console.log(article.html);
-                    // DOM
-                    console.log(article.document);
-
-                    // Response Object from Request Lib
-                    console.log(meta);
-
-                    // Close article to clean up jsdom and prevent leaks
-                    article.close();
-                });
 
             } else {
                 error(res, `${req.body.url} is not recognised as valid URL`);
